67fa9884408b389bd64f0228675690e51046712c
Affiliatethis <chris@onecrookedtree.com>
2019-11-12 16:31:29 -0500

    ClassNames added to toolbar.js div's and a blank toolbar css file will be manipulating the look of the toolbar.

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,22 +1,27 @@
 import React from "react";
 
+import "./Toolbar.css";
+
 const toolbar = props => (
-  <header>
-    <nav>
-      <div>
-        <a href="/">  THE LOGO</a>
+  <header className="toolbar">
+    <nav className="toolbar_navigation">
+      <div></div>
+      <div className="toolbar_logo">
+        <a href="/"> THE LOGO</a>
       </div>
-      <ul>
-        <li>
-          <a href="/about"> ABOUT</a>
-        </li>
-        <li>
-          <a href="/services">SERVICES</a>
-        </li>
-        <li>
-          <a href="/contact">CONTACT US</a>
-        </li>
-      </ul>
+      <div className="navigation_items">
+        <ul>
+          <li>
+            <a href="/about"> ABOUT</a>
+          </li>
+          <li>
+            <a href="/services">SERVICES</a>
+          </li>
+          <li>
+            <a href="/contact">CONTACT US</a>
+          </li>
+        </ul>
+      </div>    
     </nav>
   </header>
 );
