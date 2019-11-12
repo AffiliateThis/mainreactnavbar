c4453df64202038a682ba87459a900a2a057ec22
Affiliatethis <chris@onecrookedtree.com>
2019-11-12 16:15:54 -0500

    first commit. basic navbar added without css.

---
+++ src/components/Toolbar/Toolbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const toolbar = props => (
+  <header>
+    <nav>
+      <div>
+        <a href="/">  THE LOGO</a>
+      </div>
+      <ul>
+        <li>
+          <a href="/about"> ABOUT</a>
+        </li>
+        <li>
+          <a href="/services">SERVICES</a>
+        </li>
+        <li>
+          <a href="/contact">CONTACT US</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default toolbar;
